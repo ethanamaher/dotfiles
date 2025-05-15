8e6d4fca00bc110bffdfebf2a906e845c05658be
ethanamaher <ethanamaher@gmail.com>
2025-05-14 22:34:38 -0500

    remaps

--- nvim/lua/maherea/remap.lua
+++ nvim/lua/maherea/remap.lua
@@ -14,8 +14,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("n", "=ap", "ma=ap'a")
+
 vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
@@ -25,13 +26,15 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- delete X lines above/below
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
 -- Ctrl+c to esc
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 --vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", function()
+    require("conform").format({ bufnr = 0 })
+end)
 
 -- search and replace all of word in
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
