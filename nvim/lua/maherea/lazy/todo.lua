df6ff0daf7300d8faf676e95c4993537f77fb92a
ethanamaher <ethanamaher@gmail.com>
2025-05-16 15:27:39 -0500

    todo sidebar stuff

--- nvim/lua/maherea/lazy/todo.lua
+++ nvim/lua/maherea/lazy/todo.lua
@@ -1,38 +1,33 @@
 return {
     {
         "ethanamaher/todo-sidebar.nvim",
+        -- "todo-sidebar.nvim",
+        -- dir = "~/personal/todo-sidebar/todo-sidebar.nvim",
 
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
 
         config = function()
-            require("todo-sidebar").setup({
-                -- uncomment lines to use custom options
-                --
-                -- define custom keyword list
-                -- keywords = { "TODO", "FIXME", "NOTE", "REVIEW", }
-                --
-                -- case sensitivity
-                -- case_sensitive = false,
-                --
-                -- max number of results in sidebar window
-                -- max_results = 500,
+            local todosidebar= require("todo-sidebar")
+            todosidebar.setup({
+                sidebar = {
+                    -- define custom keyword list
+                    keywords = { "TODO", "FIXME", "NOTE", "REVIEW", },
 
-                -- sidebar = {
-                --    width = 40,
-                --     position = "right",
-                --    auto_focus = false,
-                --     auto_close_on_jump = false,
-                --     keymaps = {
-                --         jmp_to = "<CR>",
-                --         -- TODO add keybinds for various things
-                --   },
-                --},
+                    -- case sensitivity
+                    case_sensitive = true,
+
+                    -- max number of results in sidebar window
+                    -- max_results = 500,
+
+                    width = 50,
+                    position = "left",
+                    auto_focus = false,
+                    auto_close_on_jump = false,
+                },
+                vim.keymap.set("n", "<leader>tst", function() todosidebar.sidebar:toggle() end)
             })
-
-            vim.keymap.set("n", "<leader>tst", "<cmd>TodoSidebarToggle<CR>",
-                        { desc = "Toggle Todo Sidebar" })
         end
     }
 }
