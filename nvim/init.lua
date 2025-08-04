8d3aefe0c3fba32f61ef3b14d98c84e0e27523d2
ethanamaher <ethanamaher@gmail.com>
2025-08-04 12:23:17 -0500

    updated init.lua to use mason and treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,43 @@
+vim.o.winborder = 'rounded'
+vim.o.tabstop = 2
+vim.o.cursorcolumn = false
+vim.o.ignorecase = true
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.wrap = false
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.o.wrap = false
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = false
-vim.o.smartindent = true
 vim.o.swapfile = false
-vim.o.backup = false
-vim.o.undodir = os.getenv("HOME") .. "/.nvim/undo"
-vim.o.undofile = true
-vim.o.hlsearch = false
-vim.o.incsearch = true
 vim.o.termguicolors = true
-vim.g.mapleader = ' '
-vim.o.winborder = 'rounded'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.signcolumn = "yes"
 vim.o.statusline = "[%n] %{&ff}%y %<%F %=row:%l/%L col:%c %m%r%w"
 
+vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/ethanamaher/todo-sidebar.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client == nil then
+			return nil
+		end
 		if client:supports_method('textDocument/completion') then
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
@@ -43,25 +45,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 vim.cmd("set completeopt+=noselect")
 
+require "mason".setup()
 require "mini.pick".setup()
 require "oil".setup()
-
-require "gruvbox".setup({
-	italic = {
-		strings = false
-	}
-})
-require "vague".setup({
-	style = {
-		strings = "none"
-	}
+require "nvim-treesitter.configs".setup({
+	ensure_installed = { "python", "lua" },
+	highlight = { enable = true }
 })
 
-local todosidebar = require "todo-sidebar"
-todosidebar.setup()
-vim.keymap.set('n', '<leader>sb', function() todosidebar.sidebar:toggle() end)
-
-vim.lsp.enable({ "lua_ls", "basedpyright", "ruff" })
+vim.lsp.enable({ "lua_ls", "basedpyright", "ruff", "tinymist" })
 
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
@@ -69,5 +61,13 @@ vim.keymap.set('n', '<leader>e', ":Oil<CR>")
 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
+require "gruvbox".setup({
+	italic = {
+		strings = false
+	}
+})
+require "vague".setup({
+	transparent = true,
+})
 vim.cmd("colorscheme gruvbox")
 vim.cmd(":hi statusline guibg=NONE")
